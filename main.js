2605e9acddec47daf8f6e6a0daa9b54a4b729001
Kristi Smith <whippedcreem@gmail.com>
2023-06-04 10:16:13 -0400

    clock

---
+++ main.js
@@ -0,0 +1,54 @@
+
+//encapsulation
+let clock = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    setHours: function(h) {this.hours = h;},
+    setMinutes: function(m) {this.minutes = m;},
+    setSeconds: function(s) {this.seconds = s;}
+};
+
+let now = new Date()
+clock.setHours(now.getHours());
+clock.setMinutes(now.getMinutes());
+clock.setSeconds(now.getSeconds());
+console.log(clock)
+
+function formatTime(time)   {
+    return time < 10 ? '0' + time: time;
+}
+
+//intervals
+setInterval(function() {
+    clock.seconds++;
+if (clock.seconds === 60) {
+    clock.seconds = 0;
+    clock.minutes++;
+}
+if (clock.minutes === 60) {
+    clock.minutes = 0;
+    clock.hours++;
+}
+
+if (clock.hours === 24) {
+    clock.hours = 0;
+}
+document.getElementById('clock').innerHTML = formatTime(clock.hours) + ":" + formatTime(clock.minutes) + ":" + formatTime(clock.seconds);
+
+}, 1000);
+
+//console.log(clock);
+
+document.getElementById('remove').onclick = function() {
+    let element = document.getElementById('clock');
+    element.parentNode.removeChild(element);
+};
+
+let clockElement = document.querySelector("#clock");
+
+document.getElementById('reset').onclick = function() {
+    clock.hours = 0;
+    clock.minutes = 0;
+    clock.seconds = 0;
+};
